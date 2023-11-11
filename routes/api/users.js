0b69b3b0eb215af22a51f2ea0a8fa8c209cf1324
justbenuk <justbenuk@gmail.com>
2023-11-11 18:43:24 +0000

    initial commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,68 +1,80 @@
-const express = require('express')
+const express = require( 'express' )
 const router = express.Router()
-const { check, validationResult } = require('express-validator')
-const gravatar = require('gravatar')
-const bcrypt = require('bcryptjs')
+const { check, validationResult } = require( 'express-validator' )
+const gravatar = require( 'gravatar' )
+const bcrypt = require( 'bcryptjs' )
+
 //Models
-const User = require('../../models/User')
+const User = require( '../../models/User' )
 
-//@route -> POST api/users
-//@desc -> register a user
-//@access -> Public
-router.post('/', [
-  check('name', 'Name is required').not().isEmpty(),
-  check('email', 'Please neter a valid email').isEmail(),
-  check('password', 'Please enter a password with 6 or more charicters').isLength({ min: 6 })
-], async (req, res) => {
-  const errors = validationResult(req)
+/*
+?   @param GET api/users
+?   @desc Normally you would get a list oif users, im using it to test the endpoint
+?   @access Public
+*/
+router.get( '/', ( req, res ) => {
+  res.status( 200 ).json( {
+    msg: "Users API is working"
+  } )
+} )
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
+/*
+?   @param POST api/users
+?   @desc register a user
+?   @access Public
+*/
+router.post( '/', [
+  check( 'name', 'Name is required' ).not().isEmpty(),
+  check( 'email', 'Please enter a valid email' ).isEmail(),
+  check( 'password', 'Please enter a password with 6 or more characters' ).isLength( { min: 6 } )
+], async ( req, res ) => {
+  const errors = validationResult( req )
+
+  //? Form Validation
+  if ( !errors.isEmpty() ) {
+    return res.status( 400 ).json( { errors: errors.array() } )
   }
 
   const { name, email, password } = req.body
 
+  // ? Main function for user registration
   try {
-    //check if a user exits
-    let user = await User.findOne({ email })
+    //* check if a user exits
+    let user = await User.findOne( { email } )
 
-    if (user) {
-      res.status(400).json({
-        errors: [
-          {
-            msg: "Users already registered"
-          }
-        ]
-      })
+    if ( user ) {
+      return res.status( 400 ).json( { errors: [ { msg: "User already registered" } ] } )
     }
 
-    // check if user has a gravatar
-    const avatar = gravatar.url(email, {
+    //? check if user has a gravatar
+    const avatar = gravatar.url( email, {
       s: '200',
       r: 'pg',
       d: 'mm'
-    })
+    } )
 
-    user = new User({
+    user = new User( {
       name,
       email,
       avatar,
       password
-    })
+    } )
 
-    const salt = await bcrypt.genSalt(10)
+    const salt = await bcrypt.genSalt( 10 )
 
-    //hash the password
-    user.password = await bcrypt.hash(password, salt)
+    //? hash the password
+    user.password = await bcrypt.hash( password, salt )
 
-    //save the user
+    //? save the user
     await user.save()
-    res.send('User Registered')
 
-  } catch (err) {
-    console.error(err.message)
-    res.status(500).send('Server Error')
+    res.send( 'User Registered' )
+
+  } catch ( err ) {
+    console.log( err.message )
+    res.status( 500 ).send( 'Server Error' )
   }
-})
+
+} )
 
 module.exports = router
